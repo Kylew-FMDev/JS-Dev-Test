15b75747abad6baac381227bd5329b08371e2075
Kyle Williams <kylew.fmdev@gmail.com>
2021-09-07 14:24:54 -0700

    Added Apex Charts

--- public/index.js
+++ public/index.js
@@ -1,3 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import "regenerator-runtime/runtime.js";
+
+var options = {
+    chart: {
+      type: 'bar'
+    },
+    series: [{
+      name: 'sales',
+      data: [30,40,35,50,49,60,70,91,125]
+    }],
+    xaxis: {
+      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,2000
+    ]
+    }
+  }
+  
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  
+  chart.render();
